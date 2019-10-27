dd31c8790cedcc79a2d32cd3c4c611ef24cf5148
Grant Gould <grant@drummingpictures.com>
2019-10-27 12:27:46 -0400

    First initial build test

---
+++ entry.sh
@@ -0,0 +1,16 @@
+# Generate Django Project if None Exist
+if test -f "manage.py"; then
+    django-admin startapp core
+fi
+
+# Make Nessary Migrations
+python manage.py makemigrations
+
+# Migrate Database 
+python manage.py migrate
+
+# Uncomment this line below when needing to add fixtures
+# python manage.py loaddata fixtures/data.json
+
+# Run the Server
+python manage.py runserver 0.0.0.0:8000
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Build a Django Server using Docker, Postgres, and Redis
+Batteries Included:
+- Django 
+- Postgres 
+- Redis
+- Docker
+
+## How to Build
